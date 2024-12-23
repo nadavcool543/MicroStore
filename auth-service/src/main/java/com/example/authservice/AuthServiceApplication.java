2b822c9a7b06d10346dacbad1504ac0a6f75dfa7
Nadav <nadavcool543@gmail.com>
2024-12-23 09:59:51 +0200

    delete user feature

--- auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
+++
@@ -1,7 +0,0 @@
-@SpringBootApplication
-@EnableWebSecurity
-public class AuthServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(AuthServiceApplication.class, args);
-    }
-} 
